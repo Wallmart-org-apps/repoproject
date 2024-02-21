a6e653558cf3093a21d13c4af050fc01dda3e55d
swapna account <korepuswapna123@gmail.com>
2024-02-21 19:21:07 +0000

    two files moving to repo

--- javacode.java
+++ javacode.java
@@ -1,4 +1,4 @@
 this is swapna
 today i am learning git.
 git the source code management
-
+this is moving to repo
--- python.py
+++ python.py
@@ -1,3 +1,3 @@
 this is the 2nd file created in the github
 this is the pythin file
-
+s is moving to repo in the rr
